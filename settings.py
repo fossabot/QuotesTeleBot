3d4be4285ff1c940d0deef7942166074dba68fb1
Tuhin Kanti Pal <51857187+cachecleanerjeet@users.noreply.github.com>
2020-08-16 20:29:23 +0530

    Uploaded From Development Machine to Github

---
+++ settings.py
@@ -0,0 +1,12 @@
+import  os, random
+Bot_API_KEY = "PASTE YOUR API KEY"
+IDBOT =  Bot_API_KEY[:9]
+TELEGRAM_API = 'https://api.telegram.org/bot{token}/'.format(token=Bot_API_KEY)
+FIX = [{
+	'quotes': 'quote', 
+	'quote': 'quote',
+	'help': 'help', 
+	'start': 'help', 
+	'about': 'about',
+	'ping': 'ping',
+	}]

fd088e01f132a841da0c0915c4d005f000d38461
Tuhin Kanti Pal <51857187+cachecleanerjeet@users.noreply.github.com>
2020-08-17 11:27:40 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 import  os, random
-Bot_API_KEY = os.environ['API_KEY']
+Bot_API_KEY = "YOUR-API-KEY"
 IDBOT =  Bot_API_KEY[:9]
 TELEGRAM_API = 'https://api.telegram.org/bot{token}/'.format(token=Bot_API_KEY)
 FIX = [{
